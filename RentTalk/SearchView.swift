e8c9f3cff23b12e5473af5490c72458ef0173561
LostPengK <1064491643@qq.com>
2016-12-19 00:04:16 +0800

    new changes

---
+++ RentTalk/SearchView.swift
@@ -0,0 +1,21 @@
+//
+//  SearchView.swift
+//  RentTalk
+//
+//  Created by pengkang on 2016/12/6.
+//  Copyright © 2016年 pengkang. All rights reserved.
+//
+
+import UIKit
+
+class SearchView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ RentTalk/TCaoHeader.swift
@@ -0,0 +1,21 @@
+//
+//  TCaoHeader.swift
+//  RentTalk
+//
+//  Created by pengkang on 2016/12/5.
+//  Copyright © 2016年 pengkang. All rights reserved.
+//
+
+import UIKit
+
+class TCaoHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
